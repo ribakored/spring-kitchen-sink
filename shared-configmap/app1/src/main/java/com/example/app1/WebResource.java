f768ae577e1b587cbd3f4aef7257cb3d048521b9
Dhananjay <dhannajay.bakore@vitaminshoppe.com>
2022-12-30 19:03:06 -0500

    Created new apps for shared configmap example

---
+++ shared-configmap/app1/src/main/java/com/example/app1/WebResource.java
@@ -0,0 +1,15 @@
+import com.example.app1.AppConfig;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class WebResource {
+    private AppConfig appConfig;
+    public WebResource(AppConfig appConfig){
+        this.appConfig = appConfig;
+    }
+    @GetMapping(value = "/propertyValues")
+    public @ResponseBody String getPropertyValues(){
+        return String.format("Timeout=%s, Message=%s",appConfig.getTimeout(),appConfig.getMessage());
+    }
+}
